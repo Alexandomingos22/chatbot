008f3b9af5f5a0662f126e71859141f90aa25aa4
alexandre domingos <alexandre@webmail.com.br>
2022-12-20 19:31:22 -0300

    Add gitignore

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -1,6 +1,7 @@
-import os
-
 from pathlib import Path
+import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,10 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w@r9i%7jh2hmbr8u_g^$u5r&mtowst(*@su!4q(u%64s+ju(os'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
